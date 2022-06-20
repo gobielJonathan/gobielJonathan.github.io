e5c661bf29d2db30837ec15552d19ce825546db2
Jonathan Gobiel <jonathangobiel13@gmail.com>
2022-06-20 18:56:50 +0700

    chore: remove unused lib and component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import "./app.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import PageProvider from "./page.context";
 
 // This default export is required in a new `pages/_app.js` file.
